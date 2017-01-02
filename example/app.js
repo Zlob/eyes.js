fd87c04ed8b56f9ba87229a8d273c4f3b3aa3973
Vlad Makin <vamakin@gmail.com>
2017-01-02 14:25:41 +0300

    use eyesPair

--- example/app.js
+++ example/app.js
@@ -7,90 +7,45 @@ require.config({
 });
 
 require(['eyes', 'jquery'], function (Eyes) {
-
         var eyes = new Eyes();
 
-        var leftHeaderEye = eyes.createEye(
-            '#header-eyes',
-            {
-                x: 25,
-                y: 0,
-                size: 50,
-                color: "white",
-                borderColor: "black",
+        var headerEyesOptions = {
+            size: 50,
+            color: "white",
+            borderColor: "black",
+            borderSize: 5,
+            type: "left",
+            //eyeball options
+            eyeball: {
+                size: 12,
+                shift: 15
+            },
+            //top eyelid options
+            topEyelid: {
+                size: 0,
+                color: "#f7b6a4",
+                borderColor: "#000000",
                 borderSize: 5,
-                type: "left",
-                //eyeball options
-                eyeball: {
-                    size: 12,
-                    shift: 15
-                },
-                //top eyelid options
-                topEyelid: {
-                    size: 0,
-                    color: "#f7b6a4",
-                    borderColor: "#000000",
-                    borderSize: 5,
-                    bottomArcRadius: 0.3,
-                    bottomArcRadiusSweep: 1,
-                    eyelashesStyle: 'loise'
-                },
-                //bottom eyelid options
-                bottomEyelid: {
-                    size: 0,
-                    color: "#f7b6a4",
-                    borderColor: "#000000",
-                    borderSize: 5,
-                    topArcRadius: 0.3,
-                    topArcRadiusSweep: 1
-                },
-                eyebrow: {
-                    rotate: 10
-                }
-            });
+                bottomArcRadius: 0.3,
+                bottomArcRadiusSweep: 1,
+                eyelashesStyle: 'loise'
+            },
+            //bottom eyelid options
+            bottomEyelid: {
+                size: 0,
+                color: "#f7b6a4",
+                borderColor: "#000000",
+                borderSize: 5,
+                topArcRadius: 0.3,
+                topArcRadiusSweep: 1
+            }
+        };
 
-        var rightHeaderEye = eyes.createEye(
-            '#header-eyes',
-            {
-                x: 125,
-                y: 0,
-                size: 50,
-                color: "white",
-                borderColor: "black",
-                borderSize: 5,
-                type: "right",
-                //eyeball options
-                eyeball: {
-                    size: 12,
-                    shift: 15
-                },
-                //top eyelid options
-                topEyelid: {
-                    size: 0,
-                    color: "#f7b6a4",
-                    borderColor: "#000000",
-                    borderSize: 5,
-                    bottomArcRadius: 0.3,
-                    bottomArcRadiusSweep: 1,
-                    eyelashesStyle: 'loise'
-                },
-                //bottom eyelid options
-                bottomEyelid: {
-                    size: 0,
-                    color: "#f7b6a4",
-                    borderColor: "#000000",
-                    borderSize: 5,
-                    topArcRadius: 0.3,
-                    topArcRadiusSweep: 1
-                },
-                eyebrow: {
-                    rotate: -10
-                }
-            });
+        var headerEyes = eyes.createEyesPair('#header-eyes', headerEyesOptions, {x: 75, y:0}, 50);
 
         window.addEventListener("mousemove", function (e) {
-            leftHeaderEye.eyeball.track({x: e.x, y: e.y});
-            rightHeaderEye.eyeball.track({x: e.x, y: e.y});
+            headerEyes.getLeftEye().eyeball.track({x: e.x, y: e.y});
+            headerEyes.getRightEye().eyeball.track({x: e.x, y: e.y});
         }, false);
 
         var firstEye = eyes.createEye(
